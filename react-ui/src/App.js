6c49ea6571d88a79fc2d32baf677fdb15d0e31c0
Nalipp <nateplipp@gmail.com>
2017-12-28 13:19:01 -0600

    deployment test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import StudySlipForm from './StudySlipForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      hi welcome!
+        ???
       </div>
     );
   }
