9a396bce2f9794e397484c863a80f1c7fe93d321
Nalipp <nateplipp@gmail.com>
2017-12-29 09:19:52 -0600

    db config

--- server/helpers/timeSlips.js
+++ server/helpers/timeSlips.js
@@ -3,7 +3,6 @@ var db = require('../models');
 exports.getTimeSlips = function(req, res){
   db.TimeSlip.find()
   .then(function(timeSlips){
-      console.log('hi', timeSlips); 
       res.json(timeSlips);
   })
   .catch(function(err){
--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
+var url;
+
 mongoose.set('debug', true);
-// mongoose.connect('mongodb://localhost/tStudy-api');
-mongoose.connect('mongodb://heroku_bjjvh2h6:jo0oosp6t55c8ju7ljepf5jgl2@ds135747.mlab.com:35747/heroku_bjjvh2h6');
+mongoose.Promise = global.Promise;
 
+if (process.env.NODE_ENV === 'dev') url = 'mongodb://localhost/tStudy-api';
+else if (process.env.NODE_ENV === 'test' ) url = 'mongodb://localhost/tStudy-api-test';
+else url = 'mongodb://heroku_bjjvh2h6:jo0oosp6t55c8ju7ljepf5jgl2@ds135747.mlab.com:35747/heroku_bjjvh2h6';
 
-mongoose.Promise = Promise;
+mongoose.connect(url, err => {
+  if (err) console.log("# Failed to connect to MongoDB ");
+  else console.log('# Connected to MongoDB', url)
+})
 
 module.exports.TimeSlip = require("./timeSlip");
