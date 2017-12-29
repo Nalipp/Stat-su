f27bd24898316d97655e6cf677ba4ca91abf1415
Nalipp <nateplipp@gmail.com>
2017-12-29 08:57:46 -0600

    turn on mlab

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/tStudy-api');
-// mongoose.connect('mongodb://heroku_bjjvh2h6:jo0oosp6t55c8ju7ljepf5jgl2@ds135747.mlab.com:35747/heroku_bjjvh2h6');
+// mongoose.connect('mongodb://localhost/tStudy-api');
+mongoose.connect('mongodb://heroku_bjjvh2h6:jo0oosp6t55c8ju7ljepf5jgl2@ds135747.mlab.com:35747/heroku_bjjvh2h6');
 
 
 mongoose.Promise = Promise;
