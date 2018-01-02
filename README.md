67f8ab396aa307f09b994563dfd86a0a1407bf61
Nalipp <nateplipp@gmail.com>
2018-01-02 12:15:16 -0600

    updated readme and seed data

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
+## What is Tstudy?
+
+TStudy is a place where aspiring developers can record what they are working on and share it with the world, Allowing you to find others who are studying what you are studying
+
+## Tecnical overview
+
+Backend api : Node app
+
+Frontend : React
+
+Deployment : heroku
+
 ## Instructions
+
+### from the root directory (api server)
 
 run dev environment
 ```
 npm run dev
+```
+
+seed the dev database
+```
+npm run seed
 ```
 
 run production environment
@@ -22,15 +41,26 @@ npm run test
 npm run test-watch (automated)
 ```
 
-## What is Tstudy?
+### from the react-ui directory (react frontend)
 
-TStudy is a place where aspiring developers can record what they are working on and share it with the world, Allowing you to find others who are studying what you are studying
+run dev environment
+```
+npm start
+```
 
-## Tecnical overview
+create a new build version 
+```
+npm run build
+```
 
-The backend api is a node app
+### common dev pattern
 
-Frontend is react
+have two servers running (the backend api and frontend react ui)
 
-Deployment is through heroku
+create a new build from inside react-ui
+
+from inside the root directory, seed the database and run the dev environemnt
+
+back inside react-ui run npm start and style the front end
+
 
--- config/seed.js
+++ config/seed.js
@@ -7,12 +7,28 @@ MongoClient.connect("mongodb://localhost/tStudy-api", function (err, database) {
     collection.drop();
     collection.insertMany(
       [
-        {language: 'JavaScript', description: 'YDKJS'},
-        {language: 'React', description: 'Udacity'},
-        {language: 'Node.js', description: 'Advanced Web Developer Bootcamp'},
-        {language: 'Sql', description: 'Launch School'},
-        {language: 'Ruby', description: 'The Pickax Book'},
-        {language: 'Sinatra', description: 'Project'},
+        {
+          language: 'JavaScript', 
+          description: 'You Don"t Know JavaScript book series, Scope and Closures Chapter 5'},
+        {
+          language: 'React', 
+          description: 'The Advanded Web Developer Bootcamp Udemy course, created by Rithm Bootcamp in San Francisco exploring modern frontend technologies including React, Node, ES6 and D3'
+        },
+        {
+          language: 'Node.js', 
+          description: 'Complete Web Developer Bootcamp course, taught by former Galvanize Bootcamp instructor Colt Steele covering the fundamentals of web development including html, css, javaScript, jQuery, Node.js, MongoDb, mongoose'},
+        {
+          language: 'Sql', 
+          description: 'Launch School online web developer traning school, Programming and Back-end Development section 180 SQL and Relational Databases'
+        },
+        {
+          language: 'Ruby', 
+          description: 'Udacity Nano Degree, exploring the fundamentals of writing and reading from a database using the ruby language'
+        },
+        {
+          language: 'Sinatra',
+          description: 'Project exploring web development through the sinatra framework'
+        },
       ]
     );
     console.log('Seeding complete!');
